8b19965996dc9e300f4af3ecb9758c9d5bfa6d43
Stephanie smith <smithse4@gmail.com>
2020-05-07 19:21:41 -0400

    server.js and react boiler plate

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  // app.use(express.static("client/build"));
+    app.use(express.static("client/public"));
+}
+
+// Define API routes here
+app.get("/api/config", (req, res) => {
+  res.json({
+    success: true,
+    currentPort: PORT,
+  });
+});
+
+// Send every other request to the React app
+// Define any API routes before this runs
+app.get("*", (req, res) => {
+  // res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  res.sendFile(path.join(__dirname, "./client/public/index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
